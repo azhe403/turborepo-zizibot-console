8024999c6f32c62af240b545bb23855d5565f1f0
Azhe Kun <root@corp.azhe.my.id>
2024-12-12 22:56:33 +0700

    fix pendekin redirect

--- apps/pendekin-router/src/index.ts
+++ apps/pendekin-router/src/index.ts
@@ -1,7 +1,20 @@
-import { useGetPendekin } from '@zizibot/rest-client/internal/pendekin-rest';
+import { ApiResponse } from '@zizibot/contracts/rest-api/api-response';
+import { PendekinItemDetail } from '@zizibot/contracts/rest-api/pendekin';
+import { deconstructResponse } from '@zizibot/rest-client/utils/http-client';
+import axios from 'axios';
 import { Hono } from 'hono';
+import { env } from 'hono/adapter';
 
 const app = new Hono();
+
+const axiosClient = (baseUrl: string) => {
+  const axiosInstance = axios.create({
+    baseURL: baseUrl
+  });
+
+  axiosInstance.defaults.validateStatus = status => status >= 200 && status <= 500;
+  return axiosInstance;
+};
 
 app.get('/', (c) => {
   return c.json({
@@ -11,7 +24,12 @@ app.get('/', (c) => {
 
 app.get('/:pendekinPath', async c => {
   const pendekinPath = c.req.param('pendekinPath');
-  const { status, data, result } = await useGetPendekin(pendekinPath);
+  const { API_BASE_URL } = env<{ API_BASE_URL: string }>(c);
+
+  const axiosInstance = axiosClient(API_BASE_URL);
+  const axiosResponse = await axiosInstance.get<ApiResponse<PendekinItemDetail>>(`/api/pendekin/${pendekinPath}`);
+  const { status, result, data } = deconstructResponse(axiosResponse);
+
   if (status === 200) {
     return c.redirect(result.originalUrl);
   }
