c0d6d87b07de1d523f4425d9cc74113c307b1d76
Azhe Kun <root@corp.azhe.my.id>
2024-12-27 23:33:24 +0700

    redirect pendekin

--- apps/pendekin-router/api/index.ts
+++ apps/pendekin-router/api/index.ts
@@ -1,6 +1,8 @@
 require('dotenv').config();
 
 const express = require('express');
+const axios = require('axios');
+
 const app = express();
 
 app.use(express.static('public'));
@@ -10,9 +12,28 @@ app.get('/', (req, res) => {
 });
 
 app.get('/:pendekinPath', async (req, res) => {
-  res.json({
-    message: 'Route not found!'
-  });
+  const { pendekinPath } = req.params;
+
+  let config = {
+    method: 'get',
+    url: 'https://console-stg.zizibot.nf.azhe.my.id/api/pendekin/' + pendekinPath,
+    validateStatus: function() {
+      return true; // Accept all HTTP status codes
+    }
+  };
+
+  const response = await axios(config);
+  console.log('pendekin response', JSON.stringify(response.data));
+  const originalUrl = response.data.result?.originalUrl;
+  console.log('originalUrl', originalUrl);
+
+  if (originalUrl) {
+    res.redirect(originalUrl);
+  } else {
+    res.json({
+      message: 'Pendekin not found!'
+    });
+  }
 });
 
 app.listen(7140, () => console.debug('Server ready on port 7140'));
