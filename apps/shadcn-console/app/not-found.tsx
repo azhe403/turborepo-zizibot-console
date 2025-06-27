6e345435b5381463a5e684b64f4791faaca28aff
Azhe Kun <root@corp.azhe.my.id>
2025-06-27 19:55:02 +0700

    add 404 page

---
+++ apps/shadcn-console/app/not-found.tsx
@@ -0,0 +1,13 @@
+﻿// app/404.tsx
+import MainLayout from '@zizibot/ui/layouts/main-layout';
+
+export default function NotFoundPage() {
+  return (
+    <MainLayout>
+      <div style={{ textAlign: 'left', padding: '2rem' }}>
+        <h1>404 - Page Not Found</h1>
+        <p>Sorry, the page you are looking for does not exist.</p>
+      </div>
+    </MainLayout>
+  );
+}
